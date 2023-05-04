e141cefd0940d0a3d6deffbf0a9fb8a571e84292
jssdocente <jssgarcia.docente@gmail.com>
2023-05-04 22:15:55 +0200

    Backend. Paso 4. Configuración de Prettier con ESLint

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,14 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import dbConnect from './config/mongodb.js'
 
-//Cargamos las variables de entorno
+// Cargamos las variables de entorno
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const initExpressServer = () => {
     console.log(`Express server initialized listening on ${PORT}`)
@@ -24,6 +24,3 @@ try {
 }
 
 app.listen(PORT, initExpressServer)
-
-
-
